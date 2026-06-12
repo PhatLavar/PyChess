b8b092b666229bec07a55f7347f41d2ed43920bf
PhatLavar <phat.nguyentan188@gmail.com>
2026-06-12 16:36:30 +0700

    [RESET] Reset to prepare to remake the move_validator for check, checkmate and stalemate

--- chess_engine/move_validator.py
+++ chess_engine/move_validator.py
@@ -3,25 +3,26 @@ class MoveValidator:
         self.game_state = game_state
 
 
-    def _find_king(self):
-        return self.game_state.white_king_location if self.game_state.white_to_move else self.game_state.black_king_location
+    def _find_king(self, color=None):
+        pass
 
 
-    def in_check(self):
-        king_position = self._find_king()
-
-        self.game_state.white_to_move = not self.game_state.white_to_move
-        opponent_moves = self.game_state.move.get_all_possible_moves()
-
-        self.game_state.white_to_move = not self.game_state.white_to_move
-        for move in opponent_moves:
-            target_square = move[1]
-            if target_square == king_position:
-                return True
-
-        return False
+    def _get_enemy_color(self):
+        pass
 
 
+    def _is_pinned(self):
+        pass
+
+
+    def _in_check(self):
+        pass
+
+
+
+    ####################################################################################
+    # ---------------------------- CHECKMATE, STALEMATE --------------------------------
+    ####################################################################################
     def is_checkmate(self):
         valid_move = self.game_state.move.get_valid_moves()
         return len(valid_move) == 0 and self.in_check()
